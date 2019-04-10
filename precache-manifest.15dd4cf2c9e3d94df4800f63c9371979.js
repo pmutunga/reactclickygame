c0c5c16ba386fd4218f5690d029d443e362240b4
pmutunga <paskwa.mutunga@ipsafari.com>
2019-04-09 23:16:53 -0400

    Updates

---
+++ precache-manifest.15dd4cf2c9e3d94df4800f63c9371979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "72827653b2eac9bba66537d5e9039cec",
+    "url": "/reactclickygame/static/media/blue-abstract.72827653.jpg"
+  },
+  {
+    "revision": "3bda9a3765674e2eb487",
+    "url": "/reactclickygame/static/js/runtime~main.af3f12b9.js"
+  },
+  {
+    "revision": "397b8659614409ff3b13",
+    "url": "/reactclickygame/static/js/main.0df80282.chunk.js"
+  },
+  {
+    "revision": "f30f8447abc884969c1f",
+    "url": "/reactclickygame/static/js/2.c28b1f32.chunk.js"
+  },
+  {
+    "revision": "397b8659614409ff3b13",
+    "url": "/reactclickygame/static/css/main.5d2ce604.chunk.css"
+  },
+  {
+    "revision": "f30f8447abc884969c1f",
+    "url": "/reactclickygame/static/css/2.e406afd5.chunk.css"
+  },
+  {
+    "revision": "15e42c9455bf0cab6a30ea4367b37cb3",
+    "url": "/reactclickygame/index.html"
+  }
+];
